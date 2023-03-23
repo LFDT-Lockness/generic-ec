a65125aff588964ba52736ed977d86034674e470
Denis Varlakov <cordial.egg6523@fastmail.com>
2023-03-23 14:51:55 +0000

    Impl serde for NonZero

--- generic-ec/src/non_zero/mod.rs
+++ generic-ec/src/non_zero/mod.rs
@@ -2,7 +2,10 @@ use core::iter::{Product, Sum};
 
 use subtle::{ConstantTimeEq, CtOption};
 
-use crate::{Curve, Point, Scalar};
+use crate::{
+    errors::{ZeroPoint, ZeroScalar},
+    Curve, Point, Scalar,
+};
 
 use self::definition::NonZero;
 
@@ -72,6 +75,34 @@ impl<E: Curve> NonZero<Scalar<E>> {
     }
 }
 
+impl<E: Curve> From<NonZero<Point<E>>> for Point<E> {
+    fn from(point: NonZero<Point<E>>) -> Self {
+        point.into_inner()
+    }
+}
+
+impl<E: Curve> From<NonZero<Scalar<E>>> for Scalar<E> {
+    fn from(scalar: NonZero<Scalar<E>>) -> Self {
+        scalar.into_inner()
+    }
+}
+
+impl<E: Curve> TryFrom<Point<E>> for NonZero<Point<E>> {
+    type Error = ZeroPoint;
+
+    fn try_from(point: Point<E>) -> Result<Self, Self::Error> {
+        Self::from_point(point).ok_or(ZeroPoint)
+    }
+}
+
+impl<E: Curve> TryFrom<Scalar<E>> for NonZero<Scalar<E>> {
+    type Error = ZeroScalar;
+
+    fn try_from(scalar: Scalar<E>) -> Result<Self, Self::Error> {
+        Self::from_scalar(scalar).ok_or(ZeroScalar)
+    }
+}
+
 impl<E: Curve> Sum<NonZero<Scalar<E>>> for Scalar<E> {
     fn sum<I: Iterator<Item = NonZero<Scalar<E>>>>(iter: I) -> Self {
         iter.fold(Scalar::zero(), |acc, x| acc + x)
@@ -93,5 +124,22 @@ impl<E: Curve> Product<NonZero<Scalar<E>>> for NonZero<Scalar<E>> {
 impl<'s, E: Curve> Product<&'s NonZero<Scalar<E>>> for NonZero<Scalar<E>> {
     fn product<I: Iterator<Item = &'s NonZero<Scalar<E>>>>(iter: I) -> Self {
         iter.fold(Self::one(), |acc, x| acc * x)
+    }
+}
+
+#[cfg(all(test, feature = "serde"))]
+mod non_zero_is_serializable {
+    use crate::{Curve, NonZero, Point, Scalar};
+
+    fn impls_serde<T>()
+    where
+        T: serde::Serialize + serde::de::DeserializeOwned,
+    {
+    }
+
+    #[allow(dead_code)]
+    fn ensure_non_zero_is_serde<E: Curve>() {
+        impls_serde::<NonZero<Point<E>>>();
+        impls_serde::<NonZero<Scalar<E>>>();
     }
 }
