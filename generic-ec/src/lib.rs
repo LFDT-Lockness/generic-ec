3ce6e27007797fa15d70eb2ce2f075ad9faf8d50
Denis Varlakov <cordial.egg6523@fastmail.com>
2023-07-17 14:32:44 +0000

    Add wasm support, check it on CI

--- generic-ec/src/lib.rs
+++ generic-ec/src/lib.rs
@@ -173,6 +173,10 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+// We do not use getrandom directly, we just need to enable certain features
+// when `wasm` is required
+use getrandom as _;
+
 pub use generic_ec_core as core;
 
 mod arithmetic;
